2d8d992da86dc9c80a27a23257323880ffdebb66
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2024-01-26 02:38:55 -0500

    Built through the initial website.

--- main/src/App.js
+++ main/src/App.js
@@ -4,15 +4,17 @@ import Hero from './Components/Hero';
 import Analytics from './Components/Analytics';
 import Newsletter from './Components/Newsletter';
 import Cards from './Components/Cards';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Analytics/>
-      <Newsletter/>
-      <Cards/>
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
--- main/src/Components/Hero.jsx
+++ main/src/Components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     return(
         <div className='text-white'>
             <div className ='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='text-[#00df9a] font-bold p-2'> GROWING WITH DATA ANALYTICS</p>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md: py-6'> Grow with data. </h1> 
+                <p className='text-[#00df9a] font-bold p-2'> GROWING WITH SCALABLE SOLUTIONS</p>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md: py-6'> Become a Creator. </h1> 
 
                 <div className='flex justify-center items-center'>
                     <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'> Fast, Flexible Financing for</p>
