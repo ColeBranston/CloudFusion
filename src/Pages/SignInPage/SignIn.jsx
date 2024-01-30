2138c9c70a53771fc378fba7a178ef923acd8bfc
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2024-01-30 02:28:30 -0500

    Add files via upload

---
+++ src/Pages/SignInPage/SignIn.jsx
@@ -0,0 +1,61 @@
+import bg from "../../Assets/image.jpg";
+import { FcGoogle } from "react-icons/fc";
+import { HiArrowSmallLeft } from "react-icons/hi2";
+
+const SignIn = ({returnHome}) =>{
+    
+    return(
+        <div className='w-full h-screen flex items-start'> 
+            <div className='relative w-1/2 h-full flex flex-col'>
+                <div className='absolute top-[20%] left-[10%] flex-col'>
+                    <h1 className='text-2xl text-white font-bold my-4'>Turn Your Ideas into reality</h1>
+                    <p className='text-xl text-white font-normal'>Start for free and get attractive offers from the community</p>
+                </div>
+                <img src={bg} className='w-full h-full object-cover'/>
+            </div>
+            <div className='w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center'>
+                
+                <h1 className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold'>CloudFusion.</h1>
+                <div onClick={returnHome} className='w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold'>
+                    <button className="flex rounded-md"><HiArrowSmallLeft className="w-[1.75vw] h-[100%]"/><span className="ml-[5%]">Back</span></button>
+                </div>
+
+                <div className='w-full flex flex-col max-w-[500px]'>
+                    <div className='w-full flex flex-col mb-2'>
+                        <h3 className='text-3xl font-semibold mb-2'>Login</h3>
+                        <p className='text-base mb-2'>Welcome Back! Please enter your details.</p>
+                    </div>
+                    <div className='w-full flex flex-col'>
+                        <input type='email' placeholder='Email' className='w-full text-black py-4 my-2 bg-transparent border-b border-black outline-none focus:outline-none'/>
+                        <input type='password' placeholder='Password' className='w-full text-black py-4 my-2 bg-transparent border-b border-black outline-none focus:outline-none'/>
+                    </div>
+                    <div className='w-full flex items-center justify-between'>
+                        <div className='w-full flex items-center'>
+                            <input type='checkbox' className='w-4 h-4 mr-2'/>
+                            <p className='text-sm'>Remember Me</p>
+                        </div>
+                        <p className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2'>Forgot Password</p>
+                    </div>
+                    <div className='w-full flex flex-col my-4'>
+                        <button className='w-full text-white my-2 font-semibold bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>Log In</button>
+                        <button className='w-full text-[#060606] my-2 font-semibold bg-white border border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>Register</button>
+                    </div>
+                    <div className='w-full flex items-center justify-center relative py-2'>
+                        <div className='w-full h-[1px] bg-black/40'></div>
+                        <p className='text-lg absolute text-black/80 bg-[#f5f5f5]'>or</p>
+                    </div>
+                    <div>
+                        <div className='w-full text-[#060606] my-2 font-semibold bg-white border border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+                            <FcGoogle className='size-20 h-6 pl-3'/>
+                            <p className='pr-7'>Sign In With Google</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full flex items-center justify-center'>
+                    <p className='text-sm font-normal text-[#060606]'>Don't have an account? <span className='font-semibold underline underline-offset-2 cursor-pointer'>Sign up for free</span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default SignIn;
