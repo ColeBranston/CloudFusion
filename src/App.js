28ce47485a33794a1d9c189c48b8c72dcb273fd6
obaidmm <omohiudd@uwo.ca>
2024-02-12 17:15:46 -0500

    inserted some front end and importing it things to app.js

--- src/App.js
+++ src/App.js
@@ -4,68 +4,50 @@ import TeamsPage from "./Pages/TeamsPage/TeamsPage";
 import SignInPage from "./Pages/SignInPage/SignIn";
 import Settings from "./Pages/SettingsPage/Settings"
 
-import { StripeProvider } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// changing this to your publishable key
-const stripePromise = loadStripe('your_publishable_key');
-
-// function App() {
-//   const [page, setPage] = useState("Homepage")
-//   const returnHome = () => {
-//     setPage("Homepage")
-//     console.log("Homepage")
-//   };
-//   const goTeamsPage = () => {
-//     setPage("Teams")
-    
-//   };
-//   const goSignInPage = () => {
-//     setPage("SignIn")
-    
-//   };
-//   var register = () =>{
-//     setPage('Register')
-//   }
-//   var settings = () => {
-//     setPage("Settings")
-//   }
-// 
-{/* <HomePage links = {goTeamsPage} */}
-//   return (
-
-//     <div className="App">
-//       {page === "Homepage"? <HomePage links1 = {goTeamsPage} links2 = {goSignInPage} links3={settings}/>:
-//        page==="Teams"? <TeamsPage returnHome = {returnHome}/>: 
-//        page=="SignIn"? <SignInPage returnHome={returnHome}/>: <Settings returnHome={returnHome}/>}
-
-//         {/* {page === "Homepage" && <HomePage links1={goTeamsPage} links2={goSignInPage} links3={settings} />}
-//         {page === "Teams" && <TeamsPage returnHome={returnHome} />}
-//         {page === "SignIn" && <SignInPage returnHome={returnHome} />}
-//         {page === "Settings" && <Settings returnHome={returnHome} />} */}
-//     </div>
-    
-    
-//   );
-// }
-
+import PaymentPage from "./Pages/StripePage/Payment";
+import CheckoutForm from "./Pages/StripePage/CheckoutForm"; 
+import CompletionPage from "./Pages/StripePage/Completion"; 
 
 function App() {
+  const [page, setPage] = useState("Homepage")
+  const returnHome = () => {
+    setPage("Homepage")
+    console.log("Homepage")
+  };
+  const goTeamsPage = () => {
+    setPage("Teams")
+    
+  };
+  const goSignInPage = () => {
+    setPage("SignIn")
+    
+  };
+  
+  const goPayment = () => setPage("Payment"); // Add a function to go to the payment page
+
+  var register = () =>{
+    setPage('Register')
+  }
+  var settings = () => {
+    setPage("Settings")
+  }
+{/* <HomePage links = {goTeamsPage} */}
   return (
-    <StripeProvider stripe={stripePromise}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/teams" element={<TeamsPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/settings" element={<Settings />} />
-          {/* <Route path="/payment" element={<Payment />} />
-          <Route path="/completion" element={<Completion />} /> */}
-          {/* Include other routes as needed */}
-        </Routes>
-      </BrowserRouter>
-    </StripeProvider>
+    // <div className="App">
+    //   {page === "Homepage"? <HomePage links1 = {goTeamsPage} links2 = {goSignInPage} links3={settings}/>: page==="Teams"? <TeamsPage returnHome = {returnHome}/>: page=="SignIn"? <SignInPage returnHome={returnHome}/>: <Settings returnHome={returnHome}/>}
+
+    // </div>
+
+    <div className="App">
+    {page === "Homepage" && <HomePage links1={goTeamsPage} links2={goSignInPage} links3={settings} />}
+    {page === "Teams" && <TeamsPage returnHome={returnHome} />}
+    {page === "SignIn" && <SignInPage returnHome={returnHome} />}
+    {page === "Settings" && <Settings returnHome={returnHome} />}
+    {page === "Payment" && <PaymentPage />} // Add a conditional render for Payment
+    {page === "Checkout" && <CheckoutForm />} // Add a conditional render for CheckoutForm
+    {page === "Completion" && <CompletionPage />} // Add a conditional render for CompletionPage
+    {/* Continue with conditional rendering for other pages */}
+  </div>
   );
 }
 
