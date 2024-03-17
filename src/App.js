4615abcc50bf9f23cf1c3595948b75807899dde1
obaidmm <omohiudd@uwo.ca>
2024-03-17 15:38:13 -0400

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -33,17 +33,13 @@ function App() {
   }
 {/* <HomePage links = {goTeamsPage} */}
   return (
-    // <div className="App">
-    //   {page === "Homepage"? <HomePage links1 = {goTeamsPage} links2 = {goSignInPage} links3={settings}/>: page==="Teams"? <TeamsPage returnHome = {returnHome}/>: page=="SignIn"? <SignInPage returnHome={returnHome}/>: <Settings returnHome={returnHome}/>}
-
-    // </div>
 
     <div className="App">
     {page === "Homepage" && <HomePage links1={goTeamsPage} links2={goSignInPage} links3={settings} />}
     {page === "Teams" && <TeamsPage returnHome={returnHome} />}
     {page === "SignIn" && <SignInPage returnHome={returnHome} />}
     {page === "Settings" && <Settings returnHome={returnHome} />}
-    {page === "Payment" && <Payment />} // Add a conditional render for Payment
+    {page === "Payment" && <Payment returnHome={returnHome}/>} // Add a conditional render for Payment
     {page === "Checkout" && <CheckoutForm />} // Add a conditional render for CheckoutForm
     {page === "Completion" && <CompletionPage />} // Add a conditional render for CompletionPage
     {/* Continue with conditional rendering for other pages */}
