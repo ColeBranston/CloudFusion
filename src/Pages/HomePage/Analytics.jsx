645d695b23ce8f27bf8303a61fb76217fed60ae9
Joann <157896749+WlanConfig@users.noreply.github.com>
2024-03-18 07:02:45 -0400

    Content Update

--- src/Pages/HomePage/Analytics.jsx
+++ src/Pages/HomePage/Analytics.jsx
@@ -8,13 +8,14 @@ const Analytics = ({links1}) => {
 
                 <img className='w-[500px] mx-auto my-4 bg-transparent'src={Laptop} alt='/'/>
                 <div className='flex flex-col justify-center'>
-                    <p className='text-[#bfb749] font-bold'> SEARCH FOR HIRING </p>
+                    <p className='text-[#bfb749] font-bold'> SEARCH FOR YOUR SOLUTION </p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Find Solutions That Work </h1>
 
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam facilis, 
-                        fuga molestias consequuntur cupiditate maiores magnam assumenda quaerat maxime, 
-                        repellendus totam veritatis, culpa modi velit nobis itaque unde possimus.
+                        Conntect with our experienced tech professionals who drive the results you want. 
+                        From innovative approaches to proven methodologies, we have the expertise to 
+                        address your challenges and exceed your expectations.
+                        Take the first step towards success.
                     </p>
                     <Link to='/teams' className='flex bg-black text-[#bfb749] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 justify-center'>Get Started</Link>
                         
